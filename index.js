59fb7857cd0259d5cf914cc2c4ee6c6aab6b2a56
Justin Brown <justin.j.brown@outlook.com>
2023-11-29 14:21:09 -0700

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors =  require('cors');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(cors("*"));
+
+const db = mysql.createConnection({
+  host: "container-lab-db.cu1tn9kqglty.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "admin123",
+  database: "donuts_db",
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to the MySQL database");
+});
+
+app.get("/donuts", (req, res) => {
+    const sql = "SELECT * FROM donuts";
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.render('index', { donuts: result });
+    });
+});
+
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
